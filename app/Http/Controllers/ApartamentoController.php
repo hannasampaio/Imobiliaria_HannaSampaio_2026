e843a7ad79dcce7ec42ee3ca44fd12251ae354a7
SampaioH <hannasampaio7@hotmail.com>
2026-06-11 14:19:03 +0100

    Configuração inicial e criação das entidades do projeto

---
+++ app/Http/Controllers/ApartamentoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartamento;
+use Illuminate\Http\Request;
+
+class ApartamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apartamento $apartamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apartamento $apartamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apartamento $apartamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apartamento $apartamento)
+    {
+        //
+    }
+}
